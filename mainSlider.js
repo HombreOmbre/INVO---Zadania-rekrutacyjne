43bc0890931ad922de63de00e08db17092bb9e7e
Marcin_Obrebowski <96066215+HombreOmbre@users.noreply.github.com>
2022-07-19 16:09:28 +0200

    Update mainSlider.js

--- mainSlider.js
+++ mainSlider.js
@@ -11,3 +11,54 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 5,
+  spaceBetween: 40,
+  centeredSlides: true,
+  loop: true,
+  loopFillGroupWithBlank: 0,
+  autoplay: {
+      delay: 99999999999999,
+      disableOnInteraction: false,
+      reverseDirection: true,
+  },
+
+  breakpoints: {
+      1280: {
+          slidesPerView: 5,
+      },
+
+      900: {
+          slidesPerView: 4,
+          autoplay: {
+              delay: 2000,
+              disableOnInteraction: false,
+          },
+      },
+
+      500: {
+          slidesPerView: 3,
+          autoplay: {
+              delay: 2000,
+              disableOnInteraction: false,
+          },
+      },
+
+      320: {
+          slidesPerView: 2,
+          autoplay: {
+              delay: 2000,
+              disableOnInteraction: false,
+          }
+      },
+
+      0: { 
+        slidesPerView: 1,
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false,
+      }
+      },
+  }
+});
